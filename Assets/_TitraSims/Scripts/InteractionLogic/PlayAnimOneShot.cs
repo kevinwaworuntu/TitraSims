018ad11585e650af7796b3beaf0d55025f896352
Kevin Andrew <58542199+kevinwaworuntu@users.noreply.github.com>
2026-08-22 13:01:30 +0700

    Update TBA 6 dan label TK 4

--- Assets/_TitraSims/Scripts/InteractionLogic/PlayAnimOneShot.cs
+++ Assets/_TitraSims/Scripts/InteractionLogic/PlayAnimOneShot.cs
@@ -69,7 +69,7 @@ namespace InteractionLogic
 
         private void RestoreAnimator()
         {
-            _runtimeOverride[_animationConfig.GetCustomAnimGenericClipEntryName()] = null;
+            //_runtimeOverride[_animationConfig.GetCustomAnimGenericClipEntryName()] = null;
             if (_animator != null)
             {
                 _animator.runtimeAnimatorController = _originalController;
