a68d8117ec4d73bd6509064bab92bc11b463ef51
Kevin Andrew <58542199+kevinwaworuntu@users.noreply.github.com>
2026-08-26 20:18:48 +0700

    Clean up orphaned assets, move un used anim to new folder, tweak animation in tk 2 for funnel

---
+++ Assets/_TitraSims/Scripts/InteractionLogic/DetachAttachObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace InteractionLogic
+{
+    public class DetachAttachObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject parent;
+
+        private void OnEnable()
+        {
+            Detach();
+        }
+
+        private void OnDisable()
+        {
+            Attach();
+        }
+
+        public void Attach()
+        {
+            transform.parent = parent.transform;
+        }
+        public void Detach()
+        {
+            transform.parent = null;   
+        }
+    }
+}
