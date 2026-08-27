1e30e6e5149d7e8e4e02e245edc84175bc60287f
SyafiraFitraAnnisa <syafirafitraannisa@ymail.com>
2026-08-27 13:36:43 +0700

    update cairan
     update cairan tba 2, tk 2, tk 3. update anim tba 2 juga

---
+++ Assets/_TitraSims/Scripts/InteractionLogic/PlayAnimOneShot2.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using Config;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace InteractionLogic
+{
+    public class PlayAnimOneShot2 : MonoBehaviour, IAnimationPlaybackController
+    {
+        [Header("References")]
+        [SerializeField] private Animator        _animator;
+        [SerializeField] private AnimationConfig _animationConfig; // Todo : get from game manager, once completed
+        [SerializeField] private AnimationClip   _animationClip;
+
+        [SerializeField] private UnityEvent OnComplete;
+
+        private AnimatorOverrideController _runtimeOverride;
+        private RuntimeAnimatorController  _originalController;
+        private Coroutine                  _playRoutine;
+
+        private void Awake()
+        {
+            if (_animator != null)
+            {
+                _originalController = _animator.runtimeAnimatorController;
+            }
+        }
+
+        public void Play()
+        {
+            if (_animator == null || _animationConfig?.GenericAnimController == null || _animationClip == null)
+            {
+                return;
+            }
+
+            if (_playRoutine != null)
+            {
+                StopCoroutine(_playRoutine);
+            }
+
+            _animator.enabled = true;
+
+            if (_animator.runtimeAnimatorController is AnimatorOverrideController existingOverride
+                && existingOverride.runtimeAnimatorController == _animationConfig.GenericAnimController)
+            {
+                _runtimeOverride = existingOverride;
+            }
+            else
+            {
+                _runtimeOverride = new AnimatorOverrideController(_animationConfig.GenericAnimController);
+                _animator.runtimeAnimatorController = _runtimeOverride;
+            }
+
+            _runtimeOverride[_animationConfig.GetCustomAnimGenericClipEntryName()] = _animationClip;
+
+            _animator.Play(_animationConfig.GetCustomAnimEntryStateName(), 0, 0f);
+
+            _playRoutine = StartCoroutine(WaitForCompletion());
+        }
+
+        private IEnumerator WaitForCompletion()
+        {
+            yield return new WaitForSeconds(_animationClip.length);
+
+            _playRoutine = null;
+            RestoreAnimator();
+            OnComplete?.Invoke();
+        }
+
+        private void RestoreAnimator()
+        {
+            //_runtimeOverride[_animationConfig.GetCustomAnimGenericClipEntryName()] = null;
+            if (_animator != null)
+            {
+                _animator.runtimeAnimatorController = _originalController;
+            }
+        }
+
+        /// <inheritdoc />
+        public void StopPlayback()
+        {
+            if (_playRoutine != null)
+            {
+                StopCoroutine(_playRoutine);
+                _playRoutine = null;
+            }
+
+            RestoreAnimator();
+        }
+
+        public void SetAnimationClip(AnimationClip clip)
+        {
+            _animationClip = clip;
+        }
+
+        public void TemporaryScaleOneHelper(GameObject go)
+        {
+            go.SetActive(true);
+            go.transform.localScale = Vector3.one;
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
